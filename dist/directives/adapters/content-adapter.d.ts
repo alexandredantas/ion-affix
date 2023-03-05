1f7f7bc87a1fc2357f73245165bc94605a361846
Alexandre Sousa Dantas <alexandre.dantas@yahoo.com.br>
2023-03-05 18:25:08 -0300

    publishing build

---
+++ dist/directives/adapters/content-adapter.d.ts
@@ -0,0 +1,17 @@
+import { IonAffixContainer } from '../ion-affix-container';
+import { Content } from 'ionic-angular';
+import { Observable } from 'rxjs';
+/**
+ * Adapter for ion-content.
+ *
+ * @author Jonas Zuberbuehler <jonas.zuberbuehler@gmail.com>
+ */
+export declare class ContentAdapter implements IonAffixContainer {
+    content: Content;
+    constructor(content: Content);
+    onScroll(): Observable<any>;
+    getClientTop(): number;
+    getScrollTop(): number;
+    appendFixedHeader(headerElement: any): void;
+    isScrollingDown(): boolean;
+}
---
+++ dist/directives/adapters/content-adapter.js
@@ -0,0 +1,38 @@
+/**
+ * Adapter for ion-content.
+ *
+ * @author Jonas Zuberbuehler <jonas.zuberbuehler@gmail.com>
+ */
+var /**
+ * Adapter for ion-content.
+ *
+ * @author Jonas Zuberbuehler <jonas.zuberbuehler@gmail.com>
+ */
+ContentAdapter = (function () {
+    function ContentAdapter(content) {
+        this.content = content;
+    }
+    ContentAdapter.prototype.onScroll = function () {
+        return this.content.ionScroll;
+    };
+    ContentAdapter.prototype.getClientTop = function () {
+        return this.content.getScrollElement().getBoundingClientRect().top;
+    };
+    ContentAdapter.prototype.getScrollTop = function () {
+        return this.content.getScrollElement().scrollTop;
+    };
+    ContentAdapter.prototype.appendFixedHeader = function (headerElement) {
+        this.content.getNativeElement().appendChild(headerElement);
+    };
+    ContentAdapter.prototype.isScrollingDown = function () {
+        return this.content.directionY === 'down';
+    };
+    return ContentAdapter;
+}());
+/**
+ * Adapter for ion-content.
+ *
+ * @author Jonas Zuberbuehler <jonas.zuberbuehler@gmail.com>
+ */
+export { ContentAdapter };
+//# sourceMappingURL=content-adapter.js.map
